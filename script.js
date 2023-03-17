37658acaf68a70363f379a7b966a0667e3964de6
vickychandra357 <81799094+vickychandra357@users.noreply.github.com>
2023-03-17 20:33:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+function majorityElement(nums) {
+    let count = 0;
+    let candidate = null;
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        count += (num === candidate) ? 1 : -1;
+    }
+    return candidate;
+}
